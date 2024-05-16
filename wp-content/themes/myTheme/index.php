d684192e6b94ace48187eedbe673c54337a9d606
Abdullah Bhatti <abhatti1996@gmail.com>
2024-05-16 18:58:12 +0500

    canvas move to header

--- wp-content/themes/myTheme/index.php
+++ wp-content/themes/myTheme/index.php
@@ -1,38 +1,5 @@
 <?php get_header(); ?>
            
-        <!-- offcanvas-area -->
-                <div class="offcanvas-menu">
-                <span class="menu-close"><i class="fas fa-times"></i></span>
-              <form role="search" method="get" id="searchform"   class="searchform" action="http://wordpress.zcube.in/xconsulta/">
-                                <input type="text" name="s" id="search" value="" placeholder="Search"  />
-                                <button><i class="fa fa-search"></i></button>
-                            </form>
-
-                    
-                    <div id="cssmenu3" class="menu-one-page-menu-container">
-                        <ul id="menu-one-page-menu-2" class="menu">
-                            <li class="menu-item menu-item-type-custom menu-item-object-custom"><a href="index.html">Home</a></li>
-                            <li class="menu-item menu-item-type-custom menu-item-object-custom"><a href="about.html">About Us</a></li>
-                            <li class="menu-item menu-item-type-custom menu-item-object-custom"><a href="services.html">Services</a></li>
-                             <li class="menu-item menu-item-type-custom menu-item-object-custom"><a href="pricing.html">Pricing </a></li>
-                             <li class="menu-item menu-item-type-custom menu-item-object-custom"><a href="team.html">Team </a></li>
-                             
-                             <li class="menu-item menu-item-type-custom menu-item-object-custom"><a href="projects.html">Gallery Study</a></li>
-                             <li class="menu-item menu-item-type-custom menu-item-object-custom"><a href="blog.html">Blog</a></li>
-                            <li class="menu-item menu-item-type-custom menu-item-object-custom"><a href="contact.html">Contact</a></li>
-                        </ul>
-                    </div>  
-                    
-                    <div id="cssmenu2" class="menu-one-page-menu-container">
-                        <ul id="menu-one-page-menu-1" class="menu">
-                            <li class="menu-item menu-item-type-custom menu-item-object-custom"><a href="#home"><span>+8 12 3456897</span></a></li>
-                            <li class="menu-item menu-item-type-custom menu-item-object-custom"><a href="#howitwork"><span>info@example.com</span></a></li>
-                        </ul>
-                    </div>                
-            </div>
-            <div class="offcanvas-overly"></div>
-                 <!-- offcanvas-end -->
-      
         <!-- main-area -->
         <main>
             
