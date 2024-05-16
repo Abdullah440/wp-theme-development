512ff09557f569b4490ebb19c94312019fa3b555
Abdullah Bhatti <abhatti1996@gmail.com>
2024-05-16 17:36:41 +0500

    change title

--- wp-content/themes/myTheme/header.php
+++ wp-content/themes/myTheme/header.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Capatel - Solar Energy HTML Template</title>
+        <title>WP Custom Theme Development</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
@@ -69,49 +69,13 @@
                            <div class="col-xl-7 col-lg-7">
                               
                                 <div class="main-menu text-right text-xl-right">
-                                    <nav id="mobile-menu">
-                                        <ul>
-                                            <li class="has-sub">
-												<a href="index.html">Home</a>
-												<ul>
-                                                    <li><a href="index.html">Home Page 01</a></li>
-                                                    <li><a href="index-2.html">Home Page 02</a></li>
-
-												</ul>
-											</li>
-                                            <li><a href="about.html">About Us</a></li>
-                                            <li class="has-sub"><a href="#">Pages</a>
-												<ul>
-                                                    <li><a href="projects.html">Project</a></li>
-                                                    <li><a href="projects-detail.html">Project Details</a></li>
-
-                                                    <li><a href="pricing.html">Pricing</a></li>
-                                                    <li><a href="team.html">Team</a></li>
-                                                    <li><a href="faq.html">Faq</a></li>
-                                                    <li><a href="shop.html">Shop</a></li>
-                                                    <li><a href="shop-details.html">Shop Details</a>
-												</li></ul>
-											</li>
-                                            <li class="has-sub"> 
-                                              <a href="services.html">Services</a>
-                                                <ul>
-                                                     <li><a href="services.html">Services</a></li>
-                                                    <li><a href="services-detail.html">Services Details</a></li>
-												</ul>
-                                            </li>
-                                           
-											<li class="has-sub"> 
-                                                <a href="blog.html">Blog</a>
-                                                <ul>
-                                                    <li><a href="blog.html">Blog</a></li>
-                                                    <li><a href="blog-details.html">Blog Details</a></li>
-												</ul>
-                                            </li>
-                                                                        
-										
-                                          
-                                        </ul>
-                                    </nav>
+                                    <?php
+                                    wp_nav_menu(
+                                        array(
+                                            'theme_location' => 'primary-menu',
+                                            'menu_class' => 'nav-bar'
+                                            )
+                                    )?>
                                 </div>
                             </div>   
                              <div class="col-xl-1 col-lg-1 text-center d-none d-lg-block mt-15 mb-15">
