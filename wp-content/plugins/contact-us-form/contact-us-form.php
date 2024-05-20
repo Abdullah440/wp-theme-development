19a2e911201e16b0f3eb67fc34dcda70706ce1ec
Abdullah Bhatti <abhatti1996@gmail.com>
2024-05-20 18:54:19 +0500

    Plugin Development - Create custom post type simple_contact_form

--- wp-content/plugins/contact-us-form/contact-us-form.php
+++ wp-content/plugins/contact-us-form/contact-us-form.php
@@ -6,4 +6,43 @@
 * Version: 0.1
 * Author: Abdullah Bhatti
 * Author_URI: https://www.your-site.com/
-**/?>
+**/
+
+// Security Layer
+if(!defined('ABSPATH')){
+    echo "access denied!";
+    exit;
+}
+
+class SimpleContactForm{
+    public function __construct(){
+
+        add_action('init', array($this, 'create_custom_post_type'));
+
+    }
+
+    public function create_custom_post_type(){
+        // echo "<script>alert('Loaded!!!')</script>";
+        $args = array(
+            'public' => true,
+            'has_archive'=> true,
+            'support'=> array('title'),
+            'exclude_from_search'=> true,
+            'publicly_queryable'=> false,
+            'capability'=> 'manage_option',
+            'labels'=> array(
+                'name' => 'Contact Form',
+                'singular_name' => 'Contact Form Entry',
+            ),
+            'menu_icon' => 'dashicons-media-text',
+
+        );
+
+        register_post_type('simple_contact_form', $args);
+
+    }
+}
+
+new SimpleContactForm;
+
+?>
