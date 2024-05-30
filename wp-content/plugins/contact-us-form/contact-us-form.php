07f76f1957675fa06696f792c6804f94cce01b31
Abdullah Bhatti <abhatti1996@gmail.com>
2024-05-30 19:27:52 +0500

    update URI

--- wp-content/plugins/contact-us-form/contact-us-form.php
+++ wp-content/plugins/contact-us-form/contact-us-form.php
@@ -5,7 +5,7 @@
 * Description: Contact us form submit and fetch details (Develop for learning purpose)
 * Version: 0.1
 * Author: Abdullah Bhatti
-* Author_URI: https://www.your-site.com/
+* Author URI: https://www.linkedin.com/in/abdullahbhatti
 **/
 
 // Security Layer
